cd43b84060316cb5cbc658a9d87cd48460ff3c13
zwyeo97 <zwyeo97@hotmail.com>
2019-10-18 22:30:56 +0800

    Updated model.py again -remove both status and role entity and implement it as attribute instead

--- application/admin.py
+++ application/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Status)
-admin.site.register(Role)
 admin.site.register(User)
 admin.site.register(Project)
 admin.site.register(Sprint)
--- application/models.py
+++ application/models.py
@@ -2,69 +2,60 @@ from django.db import models
 
 # Create your models here.
 
-class Status(models.Model):
-    status_id = models.AutoField(primary_key = True)
-    status_name = models.CharField(max_length = 30, default = " ")
-    def __str__(self):
-        return f'{self.status_id} ({self.status_name})'
-
-class Role(models.Model):
-    role_id = models.AutoField(primary_key = True)
-    role_name = models.CharField(max_length = 20, default = " ")
-    def __str__(self):
-        return f'{self.role_id} ({self.role_name})'
-
 class User(models.Model):
     user_id = models.AutoField(primary_key = True)
-    username = models.CharField(max_length = 30, default = " ")
+    username = models.CharField(max_length = 30, default = " ", unique = True)
     password = models.CharField(max_length = 20, default = " ")
     email = models.EmailField(default = " ")
     name = models.CharField(max_length = 50, default = " ")
-    role_id = models.ForeignKey(Role, on_delete= models.CASCADE)
+    role_choice = [('SM', 'Scrum Master'), ('PO', 'Product Owner'), ('D', 'Developer')]
+    role = models.CharField(max_length = 30, choices = role_choice, default = 'D')
     def __str__ (self):
-        return f'{self.user_id} ({self.username})'
+        return f'User_id: {self.user_id} Username: {self.username}'
 
 class Project(models.Model):
     project_id = models.AutoField(primary_key = True)
     project_name = models.CharField(max_length = 100, default = " ")
     project_description = models.TextField(default = " ")
     def __str__(self):
-        return f'{self.project_id} ({self.project_name})'
+        return f'Project_id: {self.project_id} Project_name: {self.project_name}'
 
 class Sprint(models.Model):
-    sprint_id = models.AutoField(primary_key = True)
+    sprint_id = models.AutoField(primary_key =  True)
+    sprint_number = models.IntegerField(default = 1, null = True, blank=True, unique = True)
     project_id = models.ForeignKey(Project, on_delete = models.CASCADE)
     start_date = models.DateField(auto_now= True)
     end_date = models.DateField(auto_now = True)
     def __str__(self):
-        return f'{self.sprint_id} ({self.project_id})'
+        return f'Sprint {self.sprint_number}'
 
 class PBI(models.Model):
     pbi_id = models.AutoField(primary_key = True)
     project_id = models.ForeignKey(Project, on_delete = models.CASCADE)
-    sprint_id = models.ForeignKey(Sprint, on_delete = models.CASCADE, default = 0)
-    epic = models.TextField(default = " ")
+    sprint_number = models.ForeignKey(Sprint, on_delete = models.CASCADE, null = True, default = None, blank = True)
+    epic = models.TextField(default = " ", blank = True)
     user_story = models.TextField(default = " ")
     story_point = models.IntegerField(default = 0)
     priority = models.IntegerField(default = 0)
     def __str__(self):
-        return f'{self.pbi_id} ({self.user_story})'
+        return f'PBI_id:{self.pbi_id} Story: {self.user_story}'
 
 class Task(models.Model):
     task_id = models.AutoField(primary_key = True)
     pbi_id = models.ForeignKey(PBI, on_delete = models.CASCADE)
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
-    status_id = models.ForeignKey(Status, on_delete = models.CASCADE)
     task_description = models.TextField(default = " ")
     effort_hour = models.IntegerField(default = 0)
+    status_choice = [('New','Not yet started'), ('Progress', 'In progress'), ('Done', 'Completed')]
+    status = models.CharField(max_length = 50, choices = status_choice, default = 'New')
     def __str__(self):
-        return f'{self.task_id} ({self.task_description})'
+        return f'Task_id: {self.task_id} Description: {self.task_description}'
 
 class WorksOn(models.Model):
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
     project_id = models.ForeignKey(Project, on_delete = models.CASCADE, default = 0)
     def __str__(self):
-        return f'{self.user_id} ({self.project_id})'
+        return f'User: + {self.user_id} Project_ID: ({self.project_id})'
 
 
 
