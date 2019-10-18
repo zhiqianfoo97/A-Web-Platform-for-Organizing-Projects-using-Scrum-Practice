19d67263ab479d4da467a740484db8d381966344
zwyeo97 <zwyeo97@hotmail.com>
2019-10-18 18:33:33 +0800

    updated model.py after some testing

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Status)
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Sprint)
+admin.site.register(PBI)
+admin.site.register(Task)
+admin.site.register(WorksOn)
+
--- application/models.py
+++ application/models.py
@@ -4,68 +4,67 @@ from django.db import models
 
 class Status(models.Model):
     status_id = models.AutoField(primary_key = True)
-    status_name = models.CharField(max_length = 30)
+    status_name = models.CharField(max_length = 30, default = " ")
     def __str__(self):
-        return f'{self.status_id} ({self.name})'
+        return f'{self.status_id} ({self.status_name})'
 
 class Role(models.Model):
     role_id = models.AutoField(primary_key = True)
-    role_name = models.CharField(max_length = 20)
+    role_name = models.CharField(max_length = 20, default = " ")
     def __str__(self):
-        return self.role_id
+        return f'{self.role_id} ({self.role_name})'
 
 class User(models.Model):
     user_id = models.AutoField(primary_key = True)
-    username = models.CharField(max_length = 30)
-    password = models.CharField(max_length = 20)
-    email = models.EmailField
-    name = models.CharField(max_length = 50)
+    username = models.CharField(max_length = 30, default = " ")
+    password = models.CharField(max_length = 20, default = " ")
+    email = models.EmailField(default = " ")
+    name = models.CharField(max_length = 50, default = " ")
     role_id = models.ForeignKey(Role, on_delete= models.CASCADE)
     def __str__ (self):
-        return self.user_id
+        return f'{self.user_id} ({self.username})'
 
 class Project(models.Model):
     project_id = models.AutoField(primary_key = True)
-    project_name = models.CharField(max_length = 100)
-    project_description = models.TextField
+    project_name = models.CharField(max_length = 100, default = " ")
+    project_description = models.TextField(default = " ")
     def __str__(self):
         return f'{self.project_id} ({self.project_name})'
 
 class Sprint(models.Model):
     sprint_id = models.AutoField(primary_key = True)
     project_id = models.ForeignKey(Project, on_delete = models.CASCADE)
-    start_date = models.DateField
-    end_date = models.DateField
+    start_date = models.DateField(auto_now= True)
+    end_date = models.DateField(auto_now = True)
     def __str__(self):
         return f'{self.sprint_id} ({self.project_id})'
 
 class PBI(models.Model):
     pbi_id = models.AutoField(primary_key = True)
     project_id = models.ForeignKey(Project, on_delete = models.CASCADE)
-    sprint_id = models.ForeignKey(Sprint, on_delete = models.CASCADE)
-    epic = models.TextField
-    user_story = models.TextField
-    story_point = models.IntegerField
-    priority = models.IntegerField
+    sprint_id = models.ForeignKey(Sprint, on_delete = models.CASCADE, null = True, blank = True, default = 1)
+    epic = models.TextField(default = " ")
+    user_story = models.TextField(default = " ")
+    story_point = models.IntegerField(default = 0)
+    priority = models.IntegerField(default = 0)
     def __str__(self):
-        return self.pbi_id
+        return f'{self.pbi_id} ({self.user_story})'
 
 class Task(models.Model):
     task_id = models.AutoField(primary_key = True)
     pbi_id = models.ForeignKey(PBI, on_delete = models.CASCADE)
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
     status_id = models.ForeignKey(Status, on_delete = models.CASCADE)
-    task_description = models.TextField
-    effort_hour = models.IntegerField
+    task_description = models.TextField(default = " ")
+    effort_hour = models.IntegerField(default = 0)
     def __str__(self):
-        return self.task_id
+        return f'{self.task_id} ({self.task_description})'
 
 class WorksOn(models.Model):
     user_id = models.ManyToManyField(User)
     project_id = models.ManyToManyField(Project)
     def __str__(self):
         return f'{self.user_id} ({self.project_id})'
-
 
 
 
