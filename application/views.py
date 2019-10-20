1137b82b86962180db6b456d21287c48cc71c7bb
Araf Khan <55095521+ArafKhan@users.noreply.github.com>
2019-10-20 16:52:27 +0800

    fix all errors and uupdate every template to more efficient pratice

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def product_backlog_view (request,*args,**kwargs):
-    return render(request,"pb.html",{})
+    return render(request,"PB.html",{})
 
 def sprint_backlog_view (request,*args,**kwargs):
     return render(request,"SB.html",{})
