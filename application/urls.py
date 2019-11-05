4456d4c6b0cc56ed9fb4969fdf1e750c006cb164
neoys97 <neoys97@gmail.com>
2019-11-05 12:06:27 +0800

    minor variable name changes and directory restructure

--- application/urls.py
+++ application/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
-from application import views
+import application.views.product_backlog_views as pb_views
 
 
 app_name = 'application'
 urlpatterns = [
-    path('', views.BackLogList.as_view(), name = 'product-backlog-item'),
-    path('all/', views.BackLogListFullView.as_view(), name='pbi_all'),
-    path('action_page.php/', views.addData, name='addData'),
-    path('all/action_page.php/', views.addDataAll, name='addData2'),
-    path('del/', views.delData, name='delData'),
-    path('edit/', views.editData, name='editData'),
-    path('all/del/', views.delDataAll, name='delData2'),
-    path('all/edit/', views.editDataAll, name='editData2'),
-    path('inc/<int:pbi_id>/', views.increasePriority, name="increase_pri"),
-    path('decr/<int:pbi_id>/', views.decreasePriority, name="decrease_pri"),
-    path('all/inc/<int:pbi_id>/', views.increasePriorityAll, name="increase_pri2"),
-    path('all/decr/<int:pbi_id>/', views.decreasePriorityAll, name="decrease_pri2"),
+    path('', pb_views.BackLogList.as_view(), name = 'product_backlog'),
+    path('all/', pb_views.BackLogListFullView.as_view(), name='product_backlog_all'),
+    path('action_page.php/', pb_views.addData, name='addData'),
+    path('all/action_page.php/', pb_views.addDataAll, name='addData2'),
+    path('del/', pb_views.delData, name='delData'),
+    path('edit/', pb_views.editData, name='editData'),
+    path('all/del/', pb_views.delDataAll, name='delData2'),
+    path('all/edit/', pb_views.editDataAll, name='editData2'),
+    path('inc/<int:pbi_id>/', pb_views.increasePriority, name="increase_pri"),
+    path('decr/<int:pbi_id>/', pb_views.decreasePriority, name="decrease_pri"),
+    path('all/inc/<int:pbi_id>/', pb_views.increasePriorityAll, name="increase_pri2"),
+    path('all/decr/<int:pbi_id>/', pb_views.decreasePriorityAll, name="decrease_pri2"),
 ]
